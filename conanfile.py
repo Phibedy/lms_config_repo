5a8d35bc3d01b2b469286c2b7e08bc8785c399ee
phibedy <philipp.schmette@gmx.de>
2016-10-29 00:49:52 +0200

    update

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile, CMake
+
+class AutoDriveConan(ConanFile):
+    name = "imaging"
+    version = "1.0"
+    settings = None
+    exports = "README.md"
+    #conan libs you would like to use
+    requires = "lms/2.0@lms/stable"#,"cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable"
+
+    def imports(self):
+        self.copy("*.so",dst=".")
+        self.copy("*.a",dst=".")
+        self.copy("*.dylib",dst=".")
+        self.copy("bin/*",dst=".")
+        self.copy("include/*",dst="")
+        #self.copy("Eigen/*",dst="include/")
